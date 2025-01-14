4925500bbe8097145ef24c89cb57805ee45263d9
David🤴🏾 <user@users-MacBook-Air.local>
2025-01-14 23:30:52 +0100

    error solved

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,28 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'cookie': 'cookie/index.js'
+    }
+  },
   optimizeDeps: {
-    include: ['react-pdf', 'pdfjs-dist']
+    include: ['react-pdf', 'pdfjs-dist', 'cookie']
   },
   build: {
     commonjsOptions: {
-      include: [/react-pdf/, /pdfjs-dist/]
+      include: [/node_modules/],
+      transformMixedEsModules: true
     },
-    chunkSizeWarningLimit: 1600
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+          pdf: ['react-pdf', 'pdfjs-dist']
+        }
+      }
+    }
   },
   base: '/'
 })
